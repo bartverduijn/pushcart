f18fdb730f799f0acb52a1577fddaa7e1619e059
bartverduijn <23431875+bartverduijn@users.noreply.github.com>
2018-01-02 20:53:45 +0100

    basic server

---
+++ src/server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.status(200).send('CONNECTION FOUND');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}.`);
+  console.log('Press Ctrl+C to quit.');
+});
