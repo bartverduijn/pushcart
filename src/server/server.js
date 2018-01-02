c3518d663d8e2f7778776f1f1bcb8207ef7221a5
bartverduijn <23431875+bartverduijn@users.noreply.github.com>
2018-01-02 22:40:36 +0100

    style change

--- src/server/server.js
+++ src/server/server.js
@@ -7,11 +7,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.status(200).send('CONNECTION FOUND');
+    res.status(200).send('CONNECTION FOUND');
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}.`);
-  console.log('Press Ctrl+C to quit.');
+    console.log(`App listening on port ${PORT}.`);
+    console.log('Press Ctrl+C to quit.');
 });
